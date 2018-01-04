e6029c8b84f4f7c8f53431e8f177f43ba5dff83b
Colton Jenkins <jenkinscolton7@gmail.com>
2018-01-03 22:20:17 -0500

    Update for rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 MailsViewer::Engine.routes.draw do
-  controller "home" do
-    get "/index", to: :index
-    get "/raw", to: :raw
-    get "/html", to: :html
-    get "/plain", to: :plain
-    get '/attachment', to: :attachment
+  namespace "home" do
+    get :index
+    get :raw
+    get :html
+    get :plain
+    get :attachment
   end
   root to: 'home#index'
 end
